d9c1297af814ace34fab36f9d35e231ae56e2877
theslytherin <anuskasinha1200@gmail.com>
2020-06-25 01:15:40 +0530

    Fourth Phase

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 var tlFirstScroll = new TimelineMax();
 
 tlFirstScroll
-.set('.iphone-image-wrapper', {scale: 4, transformOrgin:"center top", y:"100%"})
-.to()
+.set('.iphone-image-wrapper', {scale: 4, transformOrigin:"center top"})
+.to('.iphone-image-wrapper', 3, {scale: 2.2, y:'-40%'})
+.to('.iphone-image-wrapper', 3, {scale: 1, y:'0%'})
+
+var controller = new ScrollMagic.Controller();
+
+//Scene 1
+
+var scene1 = new ScrollMagic.Scene({
+triggerElement : '.trigger1',
+triggerHook:0,
+duration:"100%"
+})
+
+.setTween(tlFirstScroll)
+.addIndicators()
+.addTo(controller);
