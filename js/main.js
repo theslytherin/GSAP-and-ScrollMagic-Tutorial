ba97fc4af00cc5324a695e5a68a91e97a64a41c6
theslytherin <anuskasinha1200@gmail.com>
2020-06-25 23:21:56 +0530

    Sixth Phase

--- js/main.js
+++ js/main.js
@@ -23,14 +23,24 @@ var tlSecondScroll = new TimelineMax();
 
 tlSecondScroll
 
-.to('.iphone1', 3,{x: "-50%"})
-.to('.iphone2', 3,{x: "50%"}, "-=3")
+.to('.iphone1', 3,{x: "-54%"})
+.to('.iphone2', 3,{x: "54%"}, "-=3")
 .from('.iphone1-text', 0.3, {autoAlpha:0}, "-=3")
 .from('.iphone2-text', 0.3, {autoAlpha:0}, "-=3")
 .to('.iphone1-text', 3,{x: "-30%"}, "-=3")
 .to('.iphone2-text', 3,{x:"30%"}, "-=3")
 
-//Scene 1
+.set('.iphone-stick', {display: "block"})
+
+.to('.iphone1-img-behind', 3,{x: "-50%"})
+.to('.iphone2-img-behind', 3,{x: "50%"}, "-=3")
+
+.to('.iphone1-img', 0.5, {autoAlpha: 0}, "-=3")
+.to('.iphone2-img', 0.5, {autoAlpha: 0}, "-=3")
+
+.to('.iphone1-text', 0.3, {autoAlpha: 0}, "-=3")
+.to('.iphone2-text', 0.3, {autoAlpha: 0}, "-=3")
+//Scene 2
 var scene1 = new ScrollMagic.Scene({
     triggerElement : '.trigger2',
     triggerHook:0,
