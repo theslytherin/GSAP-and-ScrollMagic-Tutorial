2088b194b37cd1222e4e6f8fb9f10c8c741bfce1
theslytherin <anuskasinha1200@gmail.com>
2020-06-25 18:48:16 +0530

    Fifth Phase

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var tlFirstScroll = new TimelineMax();
 
 tlFirstScroll
 .set('.iphone-image-wrapper', {scale: 4, transformOrigin:"center top"})
-.to('.iphone-image-wrapper', 3, {scale: 2.2, y:'-40%'})
+.to('.iphone-image-wrapper', 3, {scale: 2.5, y:'-40%'})
 .to('.iphone-image-wrapper', 3, {scale: 1, y:'0%'})
 
 var controller = new ScrollMagic.Controller();
@@ -16,5 +16,28 @@ duration:"100%"
 })
 
 .setTween(tlFirstScroll)
+.addIndicators()
+.addTo(controller);
+
+var tlSecondScroll = new TimelineMax();
+
+tlSecondScroll
+
+.to('.iphone1', 3,{x: "-50%"})
+.to('.iphone2', 3,{x: "50%"}, "-=3")
+.from('.iphone1-text', 0.3, {autoAlpha:0}, "-=3")
+.from('.iphone2-text', 0.3, {autoAlpha:0}, "-=3")
+.to('.iphone1-text', 3,{x: "-30%"}, "-=3")
+.to('.iphone2-text', 3,{x:"30%"}, "-=3")
+
+//Scene 1
+var scene1 = new ScrollMagic.Scene({
+    triggerElement : '.trigger2',
+    triggerHook:0,
+    duration:"100%"
+    })  
+
+.setTween(tlSecondScroll)
+.setPin(".trigger2")
 .addIndicators()
 .addTo(controller);
